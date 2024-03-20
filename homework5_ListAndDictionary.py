94957f6a9e8359e160c7b0afbf126a7fef5a77dd
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-03-20 23:32:29 +0200

    Add files via upload

---
+++ homework5_ListAndDictionary.py
@@ -0,0 +1,15 @@
+#2
+my_list = ["apple", "pear", "plum", "cherry", "peach"]
+print("List:", my_list)
+print("First element:", my_list[0])
+print("Last element:", my_list[4])
+print("Sublist:", my_list[2:6])
+my_list[2] = "orange"
+print("Modified list:", my_list, "\n")
+
+#3
+my_dict = {"apple": "яблоко", "pear": "груша", "plum": "слива", "cherry": "вишня", "peach": "персик"}
+print("Dictionary:", my_dict)
+print("Translation:", my_dict["apple"])
+my_dict["orange"] = "апельсин"
+print("Modified dictionary:", my_dict)
