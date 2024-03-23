319af254ee5a16667cefe6da35da5dd0bf44bc9c
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-03-23 21:24:25 +0200

    Errors fixed

--- homework3_StringMethods.py
+++ homework3_StringMethods.py
@@ -1,10 +1,10 @@
 #2
 my_string = input("Enter the string: ")
-print("Length:", my_string.__len__())
+print("Length:", len(my_string))
 
 #3
 print("UpperCase: ", my_string.upper())
 print("LowerCase: ", my_string.lower())
 print("Without spaces: ", my_string.replace(" ", ""))
 print("First letter: ", my_string[0])
-print("Last letter: ", my_string[my_string.__len__()-1])
+print("Last letter: ", my_string[-1])
