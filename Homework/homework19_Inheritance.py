63e7d6096cd49ad555883629b1acc2ec20852bcd
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-04-30 23:41:32 +0200

    Add files via upload

---
+++ Homework/homework19_Inheritance.py
@@ -0,0 +1,19 @@
+class Car:
+    price = 100000
+
+    def horse_powers(self):
+        return 100
+
+
+class Nissan(Car):
+    price = 230000
+
+    def horse_powers(self):
+        return 120
+
+
+class Kia(Car):
+    price = 90000
+
+    def horse_powers(self):
+        return 80
