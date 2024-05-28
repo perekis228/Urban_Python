f50e6cc8335f66862629618eeccb839abe5287aa
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-05-28 22:29:39 +0200

    Add files via upload

---
+++ Homework/homework29(9.3)_Iterator.py
@@ -0,0 +1,26 @@
+class EvenNumbers:
+    def __init__(self, start=0, end=1):
+        if start % 2 == 0:
+            self.start = start - 2
+            self.i = start - 2
+        else:
+            self.start = start - 1
+            self.i = start - 1
+        self.end = end
+
+
+    def __iter__(self):
+        self.i = self.start
+        return self
+
+    def __next__(self):
+        self.i += 2
+        if self.i < self.end:
+            return self.i
+        else:
+            raise StopIteration()
+
+
+en = EvenNumbers(10, 25)
+for i in en:
+    print(i)
