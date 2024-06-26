db0ce9cbcf360278f907f2240619ec2bf2da0e6a
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-06-26 17:04:44 +0200

    RAW

---
+++ Homework/homework35(10.4)_Queue.py
@@ -0,0 +1,65 @@
+import threading
+import queue
+import time
+
+class Table:
+    def __init__(self, number, is_busy=False):
+        self.number = number
+        self.is_busy = is_busy
+
+class Cafe(threading.Thread):
+    def __init__(self, tables, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queue = queue.Queue()
+        self.tables = tables
+
+    def customer_arrival(self):
+        for i in range(1, 20):
+            self.queue.put(i)
+            print(f'Посетитель номер {i} прибыл.', flush=True)
+            time.sleep(1)
+
+    def serve_customer(self):
+        while True:
+            try:
+                for table in tables:
+                    if not table.is_busy:
+                        cus = threading.Thread(target=Customer, args=(self.queue.get(timeout=2), table))
+                        cus.start()
+                        cus.join()
+            except queue.Empty:
+                print('END')
+                break
+                        
+class Customer(threading.Thread):
+    def __init__(self, customer, table, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.customer = customer
+        self.table = table
+
+    def run(self):
+        print(f'Посетитель номер {self.customer} сел за стол {self.table.number}.', flush=True)
+        time.sleep(5)
+        print(f'Посетитель номер {self.customer} покушал и ушёл.', flush=True)
+        self.table.is_busy = False
+
+
+# Создаем столики в кафе
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+# Инициализируем кафе
+cafe = Cafe(tables)
+
+# Запускаем потоки
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_serve_thread = threading.Thread(target=cafe.serve_customer)
+
+customer_arrival_thread.start()
+customer_serve_thread.start()
+
+# Ожидаем завершения работы прибытия посетителей
+customer_arrival_thread.join()
+customer_serve_thread.join()
