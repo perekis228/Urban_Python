c7bb518b99691840b476e952b49475cd38d6f89f
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-07-30 19:50:58 +0200

    Add files via upload

---
+++ Homework/homework40(12.2)_UnitTest/Testing.py
@@ -0,0 +1,35 @@
+import unittest
+import runner_and_tournament
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    def setUp(self):
+        self.runner_Usain = runner_and_tournament.Runner("Усейн", 10)
+        self.runner_Andrey = runner_and_tournament.Runner("Андрей", 9)
+        self.runner_Nick = runner_and_tournament.Runner("Ник", 3)
+
+    def test1(self):
+        self.tournament = runner_and_tournament.Tournament(90, self.runner_Usain, self.runner_Nick)
+        results = self.tournament.start()
+        self.assertTrue(results[2] == self.runner_Nick)
+        self.all_results.append(results)
+
+    def test2(self):
+        self.tournament = runner_and_tournament.Tournament(90, self.runner_Andrey, self.runner_Nick)
+        results = self.tournament.start()
+        self.assertTrue(results[2] == self.runner_Nick)
+        self.all_results.append(results)
+
+    def test3(self):
+        self.tournament = runner_and_tournament.Tournament(90, self.runner_Andrey ,self.runner_Usain, self.runner_Nick)
+        results = self.tournament.start()
+        self.assertTrue(results[3] == self.runner_Nick)
+        self.all_results.append(results)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results:
+            print(result)
