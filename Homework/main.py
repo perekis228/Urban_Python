bd716c8fcd7245c3ac560b1e6dffa82d1ecd2d98
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-08-29 20:31:34 +0200

    Add files via upload

---
+++ Homework/main.py
@@ -0,0 +1,22 @@
+#uvicorn main:app --reload
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+async def main() -> str:
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def admin() -> str:
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def user_number(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=1)) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("/user/{username}/{age}")
+async def user_info(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
+                    age: int = Path(ge=18, le=120, description='Enter age', example=24)) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
