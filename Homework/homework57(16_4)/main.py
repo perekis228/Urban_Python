95da24e4858dbd7b1dbddf135aea557d32547abe
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-09-07 15:35:20 +0200

    Update main.py

--- Homework/homework57(16_4)/main.py
+++ Homework/homework57(16_4)/main.py
@@ -13,30 +13,31 @@ class User(BaseModel):
     age: int
 
 @app.get("/users")
-async def all_users() -> dict:
+async def all_users() -> List[User]:
     return users
 
 @app.post("/user/{username}/{age}")
 async def create_user(username: str = Path(min_length=5, max_length=20, description='Enter username', example='Eva'),
                       age: int = Path(ge=18, le=120, description='Enter age', example=20)) -> User:
-    cur_id = int(max(users, key=int))+1 if users else 1
-    users[cur_id] = User(id=cur_id, username=username, age=age)
+    cur_id = len(users)
+    users.append(User(id=cur_id, username=username, age=age))
     return users[cur_id]
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: int = Path(description='Enter user id', example=1),
                       username: str = Path(min_length=5, max_length=20, description='Enter username', example='Eva'),
                       age: int = Path(ge=18, le=120, description='Enter age', example=20)) -> str:
-    if user_id in users.keys():
-        users[user_id] = User(id=user_id, username=username, age=age)
-        return f'User {user_id} has been updated'
-    else:
-        raise HTTPException(status_code=404, detail='User was not found')
+
+    for num, user in enumerate(users):
+        if user.id == user_id:
+            users[num] = User(id=user_id, username=username, age=age)
+            return f'User {user_id} has been updated'
+    raise HTTPException(status_code=404, detail='User was not found')
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int = Path(description='Enter user id', example=1)) -> User:
-    try:
-        del_user = users.pop(user_id)
-        return del_user
-    except KeyError:
-        raise HTTPException(status_code=404, detail='User was not found')
+    for num, user in enumerate(users):
+        if user.id == user_id:
+            del_user = users.pop(num)
+            return del_user
+    raise HTTPException(status_code=404, detail='User was not found')
